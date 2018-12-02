0963de502a0daeb446e56a5060ef7f9f7ba5d1d8
ga0404 <ga0404@21cn.com>
2018-12-02 23:47:29 +0800

    First Push

---
+++ app/src/main/java/com/login/MainActivity.java
@@ -0,0 +1,18 @@
+package com.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.administrator.helloworld.R;
+
+public class MainActivity extends BaseActivity{
+
+
+
+}
+
+
